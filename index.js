ac71f82c757eb3967b6acc1fbfd55e9c24a78899
Kyle Williams <kylecharleswilliams@gmail.com>
2020-02-07 10:18:14 -0700

    Add iteration 0

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 
+var happyMessage = ['Woot woot!', "You're awesome!", 'Your smile just cheered me up!'];
+
+var sillyMessage = 'Yaaaaaaaaas!'
+
+var sadMessage = 'Keep your head up!'
+
+
+
+function randomizer(message) {
+  var randomizer = Math.random() * 3;
+  if (randomizer < 1) {
+    return message[0]
+  } else if (randomizer > 1 && randomizer < 2){
+    return message[1]
+  } else return message[2]
+}
+
+function happyClick() {
+
+  document.querySelector('.message').innerHTML = randomizer(happyMessage);
+
+
+}
